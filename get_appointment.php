14ba7a0ab8021df807730c044c071ec30ce7153e
GIGA_ASH <130742476+Gothboi26@users.noreply.github.com>
2025-04-02 06:16:17 -0700

    Update get_appointment.php

--- get_appointment.php
+++ get_appointment.php
@@ -7,24 +7,17 @@ header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json; charset=UTF-8");
 
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(200);
-    exit();
-}
-
-if (!isset($_SESSION['user_id'])) {
-    echo json_encode(['error' => 'Unauthorized']);
-    exit();
-}
+$dsn = "mysql:host=tramway.proxy.rlwy.net;port=23857;dbname=railway";
+$username = "root";
+$password = "UjKxiGoBsHYBQMLRNjwPTMvFVFrTVLqk";
 
 try {
-    $pdo = new PDO("mysql:host=localhost;dbname=accounts", "root", "");
+    $pdo = new PDO($dsn, $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo json_encode(['error' => 'Database connection failed']);
     exit();
 }
-
 $method = $_SERVER['REQUEST_METHOD'];
 $input = json_decode(file_get_contents("php://input"), true);
 
