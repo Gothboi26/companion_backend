3b34eb24e95ea922f6f5a8850ecddfee3191e9fa
GIGA_ASH <130742476+Gothboi26@users.noreply.github.com>
2025-04-02 06:42:28 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 // Basic redirect or some logic to direct to a main page
+header("Access-Control-Allow-Origin: http://https://seniorcare-flt3.onrender.com/");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Content-Type: application/json; charset=UTF-8");
 header('Location: login.php'); // Or any page you want to load first
 exit;
 ?>
