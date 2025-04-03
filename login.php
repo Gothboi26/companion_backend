817ea5c6396414900a1612e8a836cdb1543cf4a1
GIGA_ASH <130742476+Gothboi26@users.noreply.github.com>
2025-04-03 08:59:48 +0800

    Update login.php

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 session_start(); // Start the session
 
 // ✅ CORS setup for localhost:3000
-header("Access-Control-Allow-Origin: https://seniorcare-flt3.onrender.com");
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
